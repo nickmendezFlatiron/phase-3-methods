bff64b2ca9675092700aebbebbee1cf0c2eb5ca8
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-05-11 22:42:20 -0400

    commit tree 5c07057cddb2dd522f94118d60cd1813e1f29c46

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
-# Your code here!
+
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(a , b)
+  a + b
+end
+
+def halve(n)
+  if n.class != Integer
+    return nil
+  end
+  n / 2
+end
